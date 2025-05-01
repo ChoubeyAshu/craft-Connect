5e5f9680b298157186c986182c3bd394640063b3
ChoubeyAshu <cashutosh38@gmail.com>
2025-05-01 13:33:57 +0530

    changed url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/products" element={<ProductListing />} />
             <Route path="/orders" element={<OrderManagement />} />
